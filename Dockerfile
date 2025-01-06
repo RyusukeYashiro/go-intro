0ae7c7436df9de460b1f8c2e86b921aed486ee5c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2025-01-06 10:11:02 +0000

    serverの構築、devcontaierによる環境構築完了

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ COPY go.mod go.sum ./
 COPY . .
 
 RUN go mod download && go build -o main .
+
+# Go関連ツールのインストール
+RUN go install golang.org/x/tools/gopls@latest \
+    && go install github.com/go-delve/delve/cmd/dlv@latest
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 // main.go
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+//1. 第 2 引数 req *http.Request の中身を使って、レスポンスの中身を作成する
+// 2. 作成したレスポンスの中身を、第一引数 w http.ResponseWriter に書き込む
 
 func main() {
-    fmt.Println("Hello, Docker with Golang!")
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		// ハンドラの第一引数として渡されていた http.ResponseWriter 型の変数 w に
+		// "Hello, World!"と書き込む
+		io.WriteString(w, "Hello world!\n")
+	}
+	// net/httpパッケージ内で定義されているHandleFuncを用いる。パンドラ登録作業
+	http.HandleFunc("/", helloHandler)
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
