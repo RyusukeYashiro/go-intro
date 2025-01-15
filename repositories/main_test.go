805dafca99ae6d311b1a586efefaf3dfea55b20c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2025-01-16 02:14:54 +0900

    ハンドラの修正 今までモックデータだったのをサービス層を介して通信を行えるようにする

--- repositories/main_test.go
+++ repositories/main_test.go
@@ -11,9 +11,9 @@ import (
 var testDB *sql.DB
 
 var (
-	dbUser = "docker"
-	dbPassword = "docker"
-	dbDatabase = "sampledb"
+	dbUser = os.Getenv("USER_NAME")      // DB_USER から USER_NAME に変更
+    dbPassword = os.Getenv("USER_PASS")  // DB_PASSWORD から USER_PASS に変更
+    dbDatabase = os.Getenv("DATABASE")   // DB_NAME から DATABASE に変更
 	dbConn = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,dbPassword, dbDatabase)
 )
 
